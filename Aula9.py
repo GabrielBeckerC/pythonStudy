68206e6356adf89156e42edab478ba843ddb1da3
GabrielBeckerC <gabeccosta@gmail.com>
2025-05-21 16:04:26 -0300

    aula sobre módulos e packages

---
+++ Aula9.py
@@ -0,0 +1,23 @@
+#Módulos
+
+"""
+Módulos em Python são arquivos que contém funções,
+classes e variáveis que podem ser importadas e usadas
+em outros arquivos.
+"""
+
+
+from operacoesDir.Operacoes import multiplicacao, soma
+
+
+multiplicacao(10,5)
+
+soma(5, 2)
+
+
+#Criando e importando Packages
+
+"""
+Packages são uma maneira de organizar o código em diretórios.
+Para criar um package, basta criar um diretório com um arquivo __init__.py dentro.
+"""
---
+++ operacoesDir/Operacoes.py
@@ -0,0 +1,10 @@
+
+def soma(a, b):
+  print('soma')
+  return print(a + b)
+
+
+def multiplicacao(a, b):
+  print('multiplicacao')
+  return print(a * b)
+
