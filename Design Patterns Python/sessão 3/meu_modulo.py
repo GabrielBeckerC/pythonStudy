405f6dd82edc79d585ae8d68c946e029f08aa885
GabrielBeckerC <gabeccosta@gmail.com>
2025-12-09 01:04:10 -0300

    sngleton modulo

---
+++ Design Patterns Python/sessão 3/meu_modulo.py
@@ -0,0 +1,10 @@
+def func1():
+    print('func 1')
+
+def func2():
+    print('func 2')
+
+NOME = 'Gabriel'
+
+
+print('Módulo foi importado')
---
+++ Design Patterns Python/sessão 3/singleton_modulos.py
@@ -0,0 +1,9 @@
+import meu_modulo
+
+print(f'O nome é {meu_modulo.NOME}')
+
+meu_modulo.func1()
+
+import meu_modulo as mn
+
+mn.func2()
