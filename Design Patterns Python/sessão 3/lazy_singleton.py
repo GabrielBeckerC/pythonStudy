fd85926cc9ad21e91f0005fca87f3ec69e6791c7
GabrielBeckerC <gabeccosta@gmail.com>
2025-12-09 00:56:00 -0300

    lazy singleton

---
+++ Design Patterns Python/sessão 3/lazy_singleton.py
@@ -0,0 +1,22 @@
+class Singleton:
+
+    __instance = None
+
+    def __init__(self):
+        if not Singleton.__instance:
+            print('O método __init__ foi chamado')
+        else:
+            print(f'A intancia ja foi criada: {self.get_instance()}')
+
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+s1 = Singleton() # a classe é inicializada, mas o objeto não é criado
+
+print(f'Objeto criado agora: {Singleton.get_instance()}')
+
+s2 = Singleton() # instacia já criada
